7cdca1542a8aa968316249ab76208653c704f59d
Daniel Corcoran <danielc.92@outlook.com>
2021-04-25 15:03:07 +1000

    Generate button

---
+++ src/Button/Button.tsx
@@ -0,0 +1,13 @@
+// Generated with util/create-component.js
+import React from "react";
+
+import { ButtonProps } from "./Button.types";
+
+import "./Button.scss";
+
+const Button = ({ foo }:ButtonProps) => (
+    <button data-testid="Button" className="dcui-button">{foo}</button>
+);
+
+export default Button;
+
