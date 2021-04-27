6ad28e8c49f2ebd7dec1f5aa6e78ad87fde0316f
Daniel Corcoran <danielc.92@outlook.com>
2021-04-27 21:14:29 +1000

    Worked on Layout component (Row, Column, Container)

---
+++ src/components/Layout/Layout.stories.tsx
@@ -0,0 +1,11 @@
+// Generated with util/create-component.js
+import React from "react";
+import Row from "./Layout";
+
+export default {
+  title: "Row",
+};
+
+export const WithBar = () => <Row foo="bar" />;
+
+export const WithBaz = () => <Row foo="baz" />;
---
+++ src/components/Layout/Layout.test.tsx
@@ -0,0 +1,27 @@
+// Generated with util/create-component.js
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Row from "./Layout";
+import { RowProps } from "./Row.types";
+
+describe("Test Component", () => {
+  let props: RowProps;
+
+  beforeEach(() => {
+    props = {
+      foo: "bar",
+    };
+  });
+
+  const renderComponent = () => render(<Row {...props} />);
+
+  it("should render foo text correctly", () => {
+    props.foo = "harvey was here";
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId("Row");
+
+    expect(component).toHaveTextContent("harvey was here");
+  });
+});
