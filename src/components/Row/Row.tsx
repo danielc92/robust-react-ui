cf0e8c83a1d5b461071529053258dcee2493fd78
Daniel Corcoran <danielc.92@outlook.com>
2021-04-27 20:11:04 +1000

    Generate Row

---
+++ src/components/Row/Row.tsx
@@ -0,0 +1,13 @@
+// Generated with util/create-component.js
+import React from "react";
+
+import { RowProps } from "./Row.types";
+
+import "./Row.scss";
+
+const Row: React.FC<RowProps> = ({ foo }) => (
+    <div data-testid="Row" className="foo-bar">{foo}</div>
+);
+
+export default Row;
+
