52268fc66afca6f780b0e32af6d7318392ec0dfe
Daniel Corcoran <danielc.92@outlook.com>
2021-04-28 20:46:33 +1000

    Worked on Table component and compound nodes

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,13 +1,54 @@
 // Generated with util/create-component.js
 import React from "react";
 
-import { TableProps } from "./Table.types";
+import {
+  TableBodyProps,
+  TableCellProps,
+  TableHeaderProps,
+  TableHeadProps,
+  TableProps,
+  TableRowProps,
+} from "./Table.types";
 
 import "./Table.scss";
+import classNames from "classnames";
 
-const Table: React.FC<TableProps> = ({ foo }) => (
-    <div data-testid="Table" className="foo-bar">{foo}</div>
+const Table = ({ children, striped }: TableProps) => (
+  <table
+    data-testid="Table"
+    className={classNames({
+      "dcui-table": true,
+      "dcui-table--striped": striped,
+    })}
+  >
+    {children}
+  </table>
 );
 
-export default Table;
+const Head = ({ children }: TableHeadProps) => (
+  <thead className="dcui-table__head">{children}</thead>
+);
 
+const Body = ({ children }: TableBodyProps) => (
+  <tbody className="dcui-table__body">{children}</tbody>
+);
+
+const Header = ({ children }: TableHeaderProps) => (
+  <th className="dcui-table__header">{children}</th>
+);
+
+const Row = ({ children }: TableRowProps) => (
+  <tr className="dcui-table__row">{children}</tr>
+);
+
+const Cell = ({ children }: TableCellProps) => (
+  <td className="dcui-table__cell">{children}</td>
+);
+
+Table.Row = Row;
+Table.Header = Header;
+Table.Cell = Cell;
+Table.Head = Head;
+Table.Body = Body;
+
+export default Table;
--- src/components/Table/Table.types.ts
+++ src/components/Table/Table.types.ts
@@ -1,4 +1,29 @@
+import { ReactNode } from "react";
+
 // Generated with util/create-component.js
 export interface TableProps {
-    foo: string;
+  children: ReactNode;
+  striped?: boolean;
+}
+
+export interface TableHeadProps {
+  children: ReactNode;
+}
+export interface TableRowProps {
+  children: ReactNode;
+}
+
+export interface TableCellProps {
+  children: ReactNode;
+}
+export interface TableHeaderProps {
+  children: ReactNode;
+}
+
+export interface TableHeadProps {
+  children: ReactNode;
+}
+
+export interface TableBodyProps {
+  children: ReactNode;
 }
