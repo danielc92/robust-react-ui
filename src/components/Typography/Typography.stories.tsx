f1b20084b401cb7a6135778df2fb7528161513e5
Daniel Corcoran <danielc.92@outlook.com>
2021-05-01 15:57:31 +1000

    Added some stories for Typography

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -3,9 +3,51 @@ import React from "react";
 import Typography from "./Typography";
 
 export default {
-    title: "Typography"
+  title: "Typography",
 };
 
-export const WithBar = () => <Typography foo="bar" />;
+export const HeadingLevel1 = () => (
+  <Typography.Heading level={1}>Lorem ipsum delore</Typography.Heading>
+);
+export const HeadingLevel2 = () => (
+  <Typography.Heading level={2}>Lorem ipsum delore</Typography.Heading>
+);
+export const HeadingLevel3 = () => (
+  <Typography.Heading level={3}>Lorem ipsum delore</Typography.Heading>
+);
+export const HeadingLevel4 = () => (
+  <Typography.Heading level={4}>Lorem ipsum delore</Typography.Heading>
+);
+export const HeadingLevel5 = () => (
+  <Typography.Heading level={5}>Lorem ipsum delore</Typography.Heading>
+);
+export const HeadingLevel6 = () => (
+  <Typography.Heading level={6}>Lorem ipsum delore</Typography.Heading>
+);
+export const HeadingLevel1Jumbo = () => (
+  <Typography.Heading level={1} jumbo>
+    Lorem ipsum delore
+  </Typography.Heading>
+);
 
-export const WithBaz = () => <Typography foo="baz" />;
+export const ParagraphSmall = () => (
+  <Typography.Paragraph size="small">
+    Adipisicing do quis amet sint consequat. Non anim est voluptate aliquip
+    cupidatat eu ut. Incididunt voluptate laboris aute excepteur magna eiusmod
+    nulla consectetur pariatur aliquip ea ipsum veniam commodo.
+  </Typography.Paragraph>
+);
+export const ParagraphMedium = () => (
+  <Typography.Paragraph size="medium">
+    Adipisicing do quis amet sint consequat. Non anim est voluptate aliquip
+    cupidatat eu ut. Incididunt voluptate laboris aute excepteur magna eiusmod
+    nulla consectetur pariatur aliquip ea ipsum veniam commodo.
+  </Typography.Paragraph>
+);
+export const ParagraphLarge = () => (
+  <Typography.Paragraph size="large">
+    Adipisicing do quis amet sint consequat. Non anim est voluptate aliquip
+    cupidatat eu ut. Incididunt voluptate laboris aute excepteur magna eiusmod
+    nulla consectetur pariatur aliquip ea ipsum veniam commodo.
+  </Typography.Paragraph>
+);
