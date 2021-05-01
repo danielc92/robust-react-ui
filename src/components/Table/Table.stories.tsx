db097cab5de3339ebbf06852b5193a1c3d995b33
Daniel Corcoran <danielc.92@outlook.com>
2021-05-01 16:16:07 +1000

    Created stories for Table

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -3,9 +3,150 @@ import React from "react";
 import Table from "./Table";
 
 export default {
-    title: "Table"
+  title: "Table",
 };
 
-export const WithBar = () => <Table foo="bar" />;
+export const TableBasic = () => (
+  <Table>
+    <Table.Head>
+      <Table.Row>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+      </Table.Row>
+    </Table.Head>
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
+);
 
-export const WithBaz = () => <Table foo="baz" />;
+export const TableCentered = () => (
+  <Table centerCellContent>
+    <Table.Head>
+      <Table.Row>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+      </Table.Row>
+    </Table.Head>
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
+);
+
+export const TableStriped = () => (
+  <Table striped>
+    <Table.Head>
+      <Table.Row>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+      </Table.Row>
+    </Table.Head>
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
+);
+
+export const TableWithRowSpan = () => (
+  <Table striped>
+    <Table.Head>
+      <Table.Row>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+        <Table.Header>Ipsum</Table.Header>
+      </Table.Row>
+    </Table.Head>
+    <Table.Body>
+      <Table.Row>
+        <Table.Header scope="row" rowSpan={3}>
+          Lorem
+        </Table.Header>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Header scope="row" rowSpan={2}>
+          Lipsim
+        </Table.Header>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+        <Table.Cell>Lorem</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
+);
