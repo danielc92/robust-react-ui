6f505396d6900129eb32851bfb3bfac8123e8e8d
Daniel Corcoran <danielc.92@outlook.com>
2021-05-07 19:21:45 +1000

    Baseline tests for Table

--- src/components/Table/Table.test.tsx
+++ src/components/Table/Table.test.tsx
@@ -5,23 +5,41 @@ import { render } from "@testing-library/react";
 import Table from "./Table";
 import { TableProps } from "./Table.types";
 
-describe("Test Component", () => {
+describe("Table suite", () => {
   let props: TableProps;
 
-  beforeEach(() => {
-    props = {
-      foo: "bar"
-    };
-  });
+  beforeEach(() => {});
 
-  const renderComponent = () => render(<Table {...props} />);
+  const renderComponent = () =>
+    render(
+      <Table>
+        <Table.Head>
+          <Table.Row>
+            <Table.Header>Animals</Table.Header>
+            <Table.Header>Sources</Table.Header>
+            <Table.Header>Planets</Table.Header>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Value</Table.Cell>
+            <Table.Cell>Value</Table.Cell>
+            <Table.Cell>Value</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Value</Table.Cell>
+            <Table.Cell>Value</Table.Cell>
+            <Table.Cell>Value</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    );
 
-  it("should render foo text correctly", () => {
-    props.foo = "harvey was here";
+  it("should render corret base class", () => {
     const { getByTestId } = renderComponent();
 
     const component = getByTestId("Table");
 
-    expect(component).toHaveTextContent("harvey was here");
+    expect(component).toHaveClass("dcui-table");
   });
 });
