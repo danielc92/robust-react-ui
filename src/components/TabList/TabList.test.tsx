7399bcc1b574556a81f35977215cfbb305d2686e
Daniel Corcoran <danielc.92@outlook.com>
2021-05-07 19:23:03 +1000

    Added baseline tests for TabList

--- src/components/TabList/TabList.test.tsx
+++ src/components/TabList/TabList.test.tsx
@@ -5,23 +5,38 @@ import { render } from "@testing-library/react";
 import TabList from "./TabList";
 import { TabListProps } from "./TabList.types";
 
-describe("Test Component", () => {
+describe("TabList Suite", () => {
   let props: TabListProps;
 
   beforeEach(() => {
     props = {
-      foo: "bar"
+      tabs: [
+        {
+          buttonId: "apple",
+          buttonLabel: "Apples",
+          tabContent:
+            "Irure in mollit laboris pariatur officia amet laboris aliqua duis culpa enim eu. Laborum proident occaecat ad officia esse. Nulla nostrud aliqua aute tempor. Cupidatat magna pariatur excepteur irure aliqua cillum qui laborum. Culpa consequat mollit id aute nulla irure incididunt sint ipsum irure aute.",
+          tabId: "apple-tab",
+        },
+        {
+          buttonId: "banana",
+          buttonLabel: "Banana",
+          tabContent:
+            "Irure in mollit laboris pariatur officia amet laboris aliqua duis culpa enim eu. Laborum proident occaecat ad officia esse. Nulla nostrud aliqua aute tempor. Cupidatat magna pariatur excepteur irure aliqua cillum qui laborum. Culpa consequat mollit id aute nulla irure incididunt sint ipsum irure aute.",
+          tabId: "banana-tab",
+        },
+      ],
+      ariaLabel: "Tab list",
+      fullWidth: true,
     };
   });
 
   const renderComponent = () => render(<TabList {...props} />);
 
-  it("should render foo text correctly", () => {
-    props.foo = "harvey was here";
+  it("Should render correct base class", () => {
     const { getByTestId } = renderComponent();
 
     const component = getByTestId("TabList");
-
-    expect(component).toHaveTextContent("harvey was here");
+    expect(component).toHaveClass("dcui-tablist");
   });
 });
