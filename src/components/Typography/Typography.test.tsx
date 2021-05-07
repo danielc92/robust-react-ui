d565be28eb6265fa5bf9eac509a0dda7bc94165b
Daniel Corcoran <danielc.92@outlook.com>
2021-05-07 19:23:36 +1000

    Added baseline tests for Typography.Paragraph and Heading

--- src/components/Typography/Typography.test.tsx
+++ src/components/Typography/Typography.test.tsx
@@ -3,7 +3,39 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import Typography from "./Typography";
-import { ParagraphProps } from "./Typography.types";
+import { HeadingProps, ParagraphProps } from "./Typography.types";
+
+describe("Heading Suit", () => {
+  let props: HeadingProps;
+
+  beforeEach(() => {
+    props = {
+      children: "hello world",
+      id: "heading",
+    };
+  });
+
+  const renderComponent = () => render(<Typography.Heading {...props} />);
+
+  it("should render text correct", () => {
+    const { getByTestId } = renderComponent();
+    const component = getByTestId("Heading");
+    expect(component).toHaveTextContent("hello world");
+  });
+
+  it("should contain correct base class", () => {
+    const { getByTestId } = renderComponent();
+    const component = getByTestId("Heading");
+    expect(component).toHaveClass("dcui-header");
+  });
+  it("should contain correct level class", () => {
+    const renderComponent = () =>
+      render(<Typography.Heading level={3} {...props} />);
+    const { getByTestId } = renderComponent();
+    const component = getByTestId("Heading");
+    expect(component).toHaveClass("dcui-header__3");
+  });
+});
 
 describe("Paragraph Suite", () => {
   let props: ParagraphProps;
