c5dd44f1f1fb66782b38a00aab36c2b5d6e7f175
Daniel Corcoran <danielc.92@outlook.com>
2021-05-15 20:44:28 +1000

    Created story for NavigationBar

--- src/components/NavigationBar/NavigationBar.stories.tsx
+++ src/components/NavigationBar/NavigationBar.stories.tsx
@@ -3,9 +3,82 @@ import React from "react";
 import NavigationBar from "./NavigationBar";
 
 export default {
-    title: "NavigationBar"
+  title: "NavigationBar",
 };
 
-export const WithBar = () => <NavigationBar foo="bar" />;
+const sampleLink = "https://github.com";
+const navData = [
+  {
+    id: 1,
+    parentId: null,
+    linkName: "Google",
+    linkHref: sampleLink,
+  },
+  {
+    id: 2,
+    parentId: null,
+    linkName: "Fruit Listing",
+    children: [
+      {
+        id: 6,
+        parentId: 2,
+        linkName: "Sweet Fruits",
+        children: [
+          {
+            id: 10,
+            parentId: 6,
+            linkName: "Apple",
+            linkHref: sampleLink,
+          },
+          {
+            id: 11,
+            parentId: 6,
+            linkName: "Strawberries",
+            linkHref: sampleLink,
+          },
 
-export const WithBaz = () => <NavigationBar foo="baz" />;
+          {
+            id: 13,
+            parentId: 6,
+            linkName: "Kiwi",
+            linkHref: sampleLink,
+          },
+        ],
+      },
+      {
+        id: 5,
+        parentId: 2,
+        linkName: "Sour Fruits",
+        children: [
+          {
+            id: 54,
+            parentId: 5,
+            linkName: "Lemon",
+            linkHref: sampleLink,
+          },
+        ],
+      },
+
+      {
+        id: 4,
+        parentId: 2,
+        linkName: "Other",
+        linkHref: sampleLink,
+      },
+    ],
+  },
+  {
+    id: 3,
+    parentId: null,
+    linkName: "About us",
+    linkHref: sampleLink,
+  },
+];
+
+export const Basic = () => (
+  <NavigationBar
+    data={navData}
+    ariaLabel="my navigation bar"
+    id="my navigation bar"
+  />
+);
