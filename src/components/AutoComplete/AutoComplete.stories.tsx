51dd8b7373d1a7325ac90692ec32d6f7704ac256
Daniel Corcoran <danielc.92@outlook.com>
2021-05-18 19:01:30 +1000

    Added a story for autocomplete

--- src/components/AutoComplete/AutoComplete.stories.tsx
+++ src/components/AutoComplete/AutoComplete.stories.tsx
@@ -3,9 +3,16 @@ import React from "react";
 import AutoComplete from "./AutoComplete";
 
 export default {
-    title: "AutoComplete"
+  title: "AutoComplete",
 };
 
-export const WithBar = () => <AutoComplete foo="bar" />;
-
-export const WithBaz = () => <AutoComplete foo="baz" />;
+const handleSearch = (s: string) => console.log(s);
+const options = ["Apple", "Banana", "Carrot", "Watermelon", "Pineapple"];
+export const Basic = () => (
+  <AutoComplete
+    options={options}
+    labelId="autocomplete"
+    labelText="Choose a fruit or vegetable"
+    onSearchFunction={handleSearch}
+  />
+);
