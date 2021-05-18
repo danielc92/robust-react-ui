b1c2e17ca5e89d57e7637fa24fc40e05995abf78
Daniel Corcoran <danielc.92@outlook.com>
2021-05-18 19:59:33 +1000

    Changed props to fully controlled

--- src/components/AutoComplete/AutoComplete.types.ts
+++ src/components/AutoComplete/AutoComplete.types.ts
@@ -1,7 +1,9 @@
 // Generated with util/create-component.js
 export interface AutoCompleteProps {
   options: string[];
-  onSearchFunction: (searchTerm: string) => void;
   labelText: string;
+  value: string;
   labelId: string;
+  onChangeFunction: (value: string) => void;
+  onSelectOptionFunction: (value: string) => void;
 }
