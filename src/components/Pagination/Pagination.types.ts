e9b95cf8a255cecd91cecf0c1eb323faf196ad3a
Daniel Corcoran <danielc.92@outlook.com>
2021-05-23 21:19:30 +1000

    Added type for paginationButton

--- src/components/Pagination/Pagination.types.ts
+++ src/components/Pagination/Pagination.types.ts
@@ -1,13 +1,13 @@
 // Generated with util/create-component.js
 
+export interface PaginationButtonProps {
+  current: boolean;
+
+  href: string;
+
+  pageNumber: number | string;
+}
 export interface PaginationProps {
   ariaLabel?: string;
-
-  data: {
-    current: boolean;
-
-    href: string;
-
-    pageNumber: number | string;
-  }[];
+  data: PaginationButtonProps[];
 }
