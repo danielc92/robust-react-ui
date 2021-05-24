dc2ebb2f2303ef490cb4a00eea9375ad31edb3f9
Daniel Corcoran <danielc.92@outlook.com>
2021-05-24 20:30:20 +1000

    Corrected test, and added another

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -1,11 +1,8 @@
 // Generated with util/create-component.js
 
 import React from "react";
-
 import { render } from "@testing-library/react";
-
 import Pagination from "./Pagination";
-
 import { PaginationProps } from "./Pagination.types";
 
 describe("Pagination Suite", () => {
@@ -59,14 +56,28 @@ describe("Pagination Suite", () => {
     expect(components[0]).toHaveClass("dcui-pagination__button");
   });
 
-  it("button should render the correct class", () => {
+  it("button should render with the correct text", () => {
     const { getAllByTestId } = renderComponent();
 
     const components = getAllByTestId("PaginationButton");
 
     expect(components[0]).toHaveTextContent("1");
   });
+  it("button should render with the correct text (2)", () => {
+    const { getAllByTestId } = renderComponent();
 
+    const components = getAllByTestId("PaginationButton");
+
+    expect(components[1]).toHaveTextContent("2");
+  });
+
+  it("second button should have the aria-current attribute", () => {
+    const { getAllByTestId } = renderComponent();
+
+    const components = getAllByTestId("PaginationButton");
+
+    expect(components[1]).toHaveAttribute("aria-current");
+  });
   it("second button should render the current class", () => {
     const { getAllByTestId } = renderComponent();
 
