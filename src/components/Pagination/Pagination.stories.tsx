d705b9184c622f8116fc228c7bc56c030f4d2302
Daniel Corcoran <danielc.92@outlook.com>
2021-05-24 20:57:08 +1000

    Added 2 pagination stories

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -1,11 +1,50 @@
 // Generated with util/create-component.js
+
 import React from "react";
+
 import Pagination from "./Pagination";
 
+import { PaginationProps } from "./Pagination.types";
+
 export default {
-    title: "Pagination"
+  title: "Pagination",
 };
 
-export const WithBar = () => <Pagination foo="bar" />;
+const props: PaginationProps = {
+  data: [
+    {
+      current: false,
+      href: "#",
+      pageNumber: 1,
+    },
+    {
+      current: true,
+      href: "#",
+      pageNumber: 2,
+    },
+    {
+      current: false,
+      href: "#",
+      pageNumber: 3,
+    },
+    {
+      current: false,
+      href: "#",
+      pageNumber: 4,
+    },
+    {
+      current: false,
+      href: "#",
+      pageNumber: 5,
+    },
+    {
+      current: false,
+      href: "#",
+      pageNumber: 6,
+    },
+  ],
+};
 
-export const WithBaz = () => <Pagination foo="baz" />;
+export const Basic = () => <Pagination {...props} />;
+
+export const Rounded = () => <Pagination {...props} rounded />;
