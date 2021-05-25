7688eeca0ef87956cd3e26a25db3e88e63561f78
Daniel Corcoran <danielc.92@outlook.com>
2021-05-25 21:09:50 +1000

    Accordion typing

---
+++ src/components/Accordion/Accordion.types.ts
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+export interface AccordionProps {
+  children: ReactNode;
+  activePanel: string;
+  fullWidth?: boolean;
+  onSelectPanelFunction: (panelId: string) => void;
+}
+export interface AccordionPanelProps {
+  children: ReactNode;
+  panelTitle: string;
+  panelId: string;
+  activePanel?: string;
+  _onSelectPanelFunction?: (panelId: string) => void;
+  _handleFocusPrevious?: (_index: number) => void;
+  _handleFocusNext?: (_index: number) => void;
+  _index?: number;
+}
