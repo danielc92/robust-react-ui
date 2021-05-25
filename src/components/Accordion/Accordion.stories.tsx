23e163fb369cb107d33291c688038d3e3dff8a3e
Daniel Corcoran <danielc.92@outlook.com>
2021-05-25 21:09:34 +1000

    Accordion base stories

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,9 @@
+// Generated with util/create-component.js
+import React from "react";
+import Accordian from "./Accordian";
+
+export default {
+  title: "Accordion",
+};
+
+export const Basic = () => <p>TBD</p>;
