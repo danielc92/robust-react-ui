895a940a156b07796deb111dd59ed46ecab70ee8
Daniel Corcoran <danielc.92@outlook.com>
2021-05-26 18:16:46 +1000

    Alert stories

---
+++ src/components/Alert/Alert.stories.tsx
@@ -0,0 +1,33 @@
+// Generated with util/create-component.js
+import React from "react";
+import Alert from "./Alert";
+
+export default {
+  title: "Alert",
+};
+
+export const Primary = () => (
+  <Alert variant="primary">
+    <p className="dcui-para dcui-para--medium">
+      Et consectetur ex fugiat quis ipsum id aute adipisicing magna ex ad
+      eiusmod proident adipisicing.
+    </p>
+  </Alert>
+);
+
+export const Success = () => (
+  <Alert variant="success">
+    <p className="dcui-para dcui-para--medium">
+      Excepteur voluptate excepteur occaecat magna elit.
+    </p>
+  </Alert>
+);
+
+export const Danger = () => (
+  <Alert variant="danger">
+    <p className="dcui-para dcui-para--medium">
+      Excepteur in irure aliqua et aliqua elit cupidatat mollit ipsum do eu
+      officia tempor ex.
+    </p>
+  </Alert>
+);
