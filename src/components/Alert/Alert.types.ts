371c2869f741347dbed6f064c4a148d0e94e10b7
Daniel Corcoran <danielc.92@outlook.com>
2021-05-26 18:16:38 +1000

    Alert typings

---
+++ src/components/Alert/Alert.types.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+
+// Generated with util/create-component.js
+export interface AlertProps {
+  children: ReactNode;
+  variant?: "primary" | "success" | "danger";
+  fullWidth?: boolean;
+}
