038c9b07ed0cec010e40d75c0acd523a2ee8914d
Daniel Corcoran <danielc.92@outlook.com>
2021-05-28 21:05:36 +1000

    Added more tests to AutoComplete

--- src/components/AutoComplete/AutoComplete.test.tsx
+++ src/components/AutoComplete/AutoComplete.test.tsx
@@ -1,6 +1,6 @@
 // Generated with util/create-component.js
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 import AutoComplete from "./AutoComplete";
 import { AutoCompleteProps } from "./AutoComplete.types";
@@ -28,6 +28,42 @@ describe("Autocomplete Suite", () => {
     const component = getByTestId("AutoComplete");
 
     expect(component).toHaveClass("dcui-autocomplete");
+  });
+
+  it("should render 4 list elements", () => {
+    const { getAllByTestId } = renderComponent();
+    const components = getAllByTestId("AutoCompleteResult");
+    expect(components).toHaveLength(4);
+  });
+
+  it("list results should have correct class", () => {
+    const { getAllByTestId } = renderComponent();
+    const components = getAllByTestId("AutoCompleteResult");
+    expect(components[0]).toHaveClass("dcui-autocomplete__result");
+  });
+  it("list result should have selected modifier class on mouse over event", () => {
+    const { getAllByTestId } = renderComponent();
+    const components = getAllByTestId("AutoCompleteResult");
+    fireEvent(
+      components[0],
+      new MouseEvent("mouseover", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    expect(components[0]).toHaveClass("dcui-autocomplete__result--selected");
+  });
+  it("list result should have aria-selected 'true' on mouse over event", () => {
+    const { getAllByTestId } = renderComponent();
+    const components = getAllByTestId("AutoCompleteResult");
+    fireEvent(
+      components[0],
+      new MouseEvent("mouseover", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    expect(components[0].getAttribute("aria-selected")).toEqual("true");
   });
 
   it("Label should render with correct text", () => {
