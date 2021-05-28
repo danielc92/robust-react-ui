15e503dc6ebc599c7eddcde0fdc36bde8f2fc696
Daniel Corcoran <danielc.92@outlook.com>
2021-05-29 09:58:13 +1000

    Update stories generation template

--- util/templates/component.stories.js
+++ util/templates/component.stories.js
@@ -1,15 +1,24 @@
 module.exports = (componentName) => ({
   content: `// Generated with util/create-component.js
 import React from "react";
+import { Story, Meta } from "@storybook/react";
 import ${componentName} from "./${componentName}";
+import { ${componentName}Props } from "./${componentName}.types";
 
 export default {
-    title: "${componentName}"
+  title: "${componentName}",
+  component: ${componentName},
+  args: {
+    // Insert default args here
+  },
+} as Meta<${componentName}Props>;
+
+const Template: Story<${componentName}Props> = (args) => {
+  return <${componentName} {...args} />;
 };
 
-export const WithBar = () => <${componentName} foo="bar" />;
-
-export const WithBaz = () => <${componentName} foo="baz" />;
+export const Basic: Story<${componentName}Props> = Template.bind({});
+Basic.args = {}
 `,
-  extension: `.stories.tsx`
+  extension: `.stories.tsx`,
 });
