fcd989ffe3c24317537530156934cfbeb01410af
Daniel Corcoran <danielc92@outlook.com>
2021-05-29 14:09:28 +1000

    Updated Autocomplete Stories

--- src/components/AutoComplete/AutoComplete.stories.tsx
+++ src/components/AutoComplete/AutoComplete.stories.tsx
@@ -1,18 +1,51 @@
-// Generated with util/create-component.js
-import React from "react";
+// // Generated with util/create-component.js
+
+import { Meta, Story } from "@storybook/react";
+import React, { useState } from "react";
 import AutoComplete from "./AutoComplete";
+import { AutoCompleteProps } from "./AutoComplete.types";
 
 export default {
   title: "AutoComplete",
+  component: AutoComplete,
+  args: {
+    labelId: "colour-autocomplete",
+    labelText: "Choose a colour",
+  },
+} as Meta<AutoCompleteProps>;
+
+const Template: Story<AutoCompleteProps> = (args) => {
+  const mockData = [
+    "blue",
+    "light blue",
+    "dark blue",
+    "green",
+    "yellow",
+    "red",
+    "gold",
+  ];
+  const [options, setOptions] = useState<Array<string>>([]);
+  const [value, setValue] = useState<string>("");
+  const onClearOptionsFunction = () => setOptions([]);
+  const onChangeFunction = (newValue: string) => {
+    setValue(newValue);
+    setOptions(
+      mockData.filter(
+        (o) => o.toLowerCase().indexOf(newValue.toLowerCase()) > -1
+      )
+    );
+  };
+  const onSelectOptionFunction = (newValue: string) => setValue(newValue);
+  return (
+    <AutoComplete
+      {...args}
+      options={options}
+      onClearOptionsFunction={onClearOptionsFunction}
+      onSelectOptionFunction={onSelectOptionFunction}
+      onChangeFunction={onChangeFunction}
+      value={value}
+    />
+  );
 };
 
-const handleSearch = (s: string) => console.log(s);
-const options = ["Apple", "Banana", "Carrot", "Watermelon", "Pineapple"];
-export const Basic = () => (
-  <AutoComplete
-    options={options}
-    labelId="autocomplete"
-    labelText="Choose a fruit or vegetable"
-    onSearchFunction={handleSearch}
-  />
-);
+export const Basic: Story<AutoCompleteProps> = Template.bind({});
