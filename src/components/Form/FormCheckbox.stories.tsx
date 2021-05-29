6e8438354c0d50cc5bf0e6a5ed9ca4925c794cb5
Daniel Corcoran <danielc92@outlook.com>
2021-05-29 21:22:22 +1000

    New checkbox stories

---
+++ src/components/Form/FormCheckbox.stories.tsx
@@ -0,0 +1,69 @@
+// CheckboxGroup.stories.tsx
+
+import React, { useState } from "react";
+import { Story, Meta } from "@storybook/react";
+import Form from "./Form";
+import { CheckboxOptionProps, FormCheckboxGroupProps } from "./Form.types";
+const { CheckboxGroup, Label, FieldSet } = Form;
+
+export default {
+  title: "CheckboxGroup",
+  component: CheckboxGroup,
+  args: {
+    id: "CheckboxGroup-example",
+    placeholder: "Type some text",
+    type: "text",
+    ariaLabelledBy: "checkbox-label",
+  },
+} as Meta<FormCheckboxGroupProps>;
+
+const Template: Story<FormCheckboxGroupProps> = (args) => {
+  const initialValues: CheckboxOptionProps[] = [
+    {
+      displayValue: "Go for apples",
+      keyValue: "a",
+      checked: false,
+    },
+    {
+      displayValue: "Go for watermelon",
+      keyValue: "c",
+      checked: false,
+    },
+    {
+      displayValue: "Go for oranges",
+      keyValue: "b",
+      checked: false,
+    },
+    {
+      displayValue: "Go for banans",
+      keyValue: "d",
+      checked: false,
+    },
+  ];
+
+  const [values, setValues] = useState<CheckboxOptionProps[]>(initialValues);
+
+  const onSelectFunction = (keyValue: string, checked: boolean) => {
+    setValues(
+      values.map((option) => {
+        if (option.keyValue === keyValue) {
+          return { ...option, checked };
+        }
+        return option;
+      })
+    );
+  };
+  return (
+    <FieldSet>
+      <Label id="checkbox-label">CheckboxGroup Label</Label>
+      <CheckboxGroup
+        {...args}
+        values={values}
+        onSelectFunction={onSelectFunction}
+      />
+    </FieldSet>
+  );
+};
+
+export const Basic: Story<FormCheckboxGroupProps> = Template.bind({});
+Basic.args = {};
