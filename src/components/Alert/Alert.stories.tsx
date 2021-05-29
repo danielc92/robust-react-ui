7e5809a1a8e5f6bcf6e9ac175fb23599ed3d7e6f
Daniel Corcoran <danielc92@outlook.com>
2021-05-29 12:51:51 +1000

    Updated Alert stories

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -1,33 +1,33 @@
 // Generated with util/create-component.js
 import React from "react";
 import Alert from "./Alert";
+import { Meta, Story } from "@storybook/react";
+import { AlertProps } from "./Alert.types";
 
 export default {
   title: "Alert",
+  component: Alert,
+  args: {},
+} as Meta<AlertProps>;
+
+const Template: Story<AlertProps> = (args) => {
+  return (
+    <Alert {...args}>
+      <p className="dcui-para dcui-para--medium">
+        Et consectetur ex fugiat quis ipsum id aute adipisicing magna ex ad
+        eiusmod proident adipisicing.
+      </p>
+    </Alert>
+  );
 };
 
-export const Primary = () => (
-  <Alert variant="primary">
-    <p className="dcui-para dcui-para--medium">
-      Et consectetur ex fugiat quis ipsum id aute adipisicing magna ex ad
-      eiusmod proident adipisicing.
-    </p>
-  </Alert>
-);
-
-export const Success = () => (
-  <Alert variant="success">
-    <p className="dcui-para dcui-para--medium">
-      Excepteur voluptate excepteur occaecat magna elit.
-    </p>
-  </Alert>
-);
-
-export const Danger = () => (
-  <Alert variant="danger">
-    <p className="dcui-para dcui-para--medium">
-      Excepteur in irure aliqua et aliqua elit cupidatat mollit ipsum do eu
-      officia tempor ex.
-    </p>
-  </Alert>
-);
+export const Primary: Story<AlertProps> = Template.bind({});
+Primary.args = {};
+export const Danger: Story<AlertProps> = Template.bind({});
+Danger.args = {
+  variant: "danger",
+};
+export const Success: Story<AlertProps> = Template.bind({});
+Success.args = {
+  variant: "success",
+};
