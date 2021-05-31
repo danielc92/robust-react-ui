bc650216668b8448233516e0d067bcece29fe1bc
Daniel Corcoran <danielc92@outlook.com>
2021-05-31 21:24:05 +1000

    Updated Link stories

--- src/components/Link/Link.stories.tsx
+++ src/components/Link/Link.stories.tsx
@@ -2,15 +2,28 @@
 import React from "react";
 import Link from "./Link";
 import { LinkProps } from "./Link.types";
+import { Meta, Story } from "@storybook/react";
 
 export default {
   title: "Components/Link",
+  args: {
+    children: "learn more about",
+    href: "https://github.com/danielc92/dc-react-ui",
+    ariaLabel: "daniels github repo",
+  },
+} as Meta<LinkProps>;
+
+const Template: Story<LinkProps> = (args) => <Link {...args} />;
+
+export const Small: Story<LinkProps> = Template.bind({});
+Small.args = {
+  size: "small",
 };
 
-const props: LinkProps = {
-  children: "learn more about",
-  href: "https://github.com/danielc92/dc-react-ui",
-  ariaLabel: "daniels github repo",
-};
+export const Medium: Story<LinkProps> = Template.bind({});
+Medium.args = {};
 
-export const Basic = () => <Link {...props} />;
+export const Large: Story<LinkProps> = Template.bind({});
+Large.args = {
+  size: "large",
+};
