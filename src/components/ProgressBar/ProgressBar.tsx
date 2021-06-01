ff72d269b7367ebeb42a4fc2153ec9b2bdf64059
Daniel Corcoran <danielc92@outlook.com>
2021-06-01 20:36:58 +1000

    Basic functionality for progress bar

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,45 @@
+// Generated with util/create-component.js
+import React from "react";
+
+import { ProgressBarProps } from "./ProgressBar.types";
+
+import "./ProgressBar.scss";
+import classNames from "classnames";
+
+const ProgressBar = ({
+  minimumValue,
+  maximumValue,
+  currentValue,
+  size,
+  fullWidth,
+  variant,
+}: ProgressBarProps) => (
+  <div
+    className={classNames({
+      "dcui-progress": true,
+      "dcui-progress--full-width": fullWidth,
+      "dcui-progress--small": size === "small",
+      "dcui-progress--medium": size === "medium" || !size,
+      "dcui-progress--large": size === "large",
+      "dcui-progress--primary": !variant || variant === "primary",
+      "dcui-progress--danger": variant === "danger",
+      "dcui-progress--secondary": variant === "secondary",
+      "dcui-progress--success": variant === "success",
+    })}
+    data-testid="ProgressBar"
+  >
+    <div
+      data-testid="ProgressBarInner"
+      className="dcui-progress__inner"
+      role="progressbar"
+      aria-valuenow={currentValue || 0}
+      aria-valuemin={minimumValue || 0}
+      aria-valuemax={maximumValue || 100}
+      style={{ width: currentValue.toString() + "%" }}
+    >
+      {/* <span className="sr-only">{currentValue.toString() + "% Complete"}</span> */}
+    </div>
+  </div>
+);
+
+export default ProgressBar;
