5847ba7b8e00e166e33b8a646f9d8cfc9ba33a5c
Daniel Corcoran <danielc92@outlook.com>
2021-06-01 20:45:04 +1000

    Added more unit tests

--- src/components/ProgressBar/ProgressBar.test.tsx
+++ src/components/ProgressBar/ProgressBar.test.tsx
@@ -10,7 +10,7 @@ describe("Progress Suite", () => {
 
   beforeEach(() => {
     props = {
-      currentValue: 50;
+      currentValue: 50,
     };
   });
 
@@ -44,5 +44,41 @@ describe("Progress Suite", () => {
     const component = getByTestId("ProgressBarInner");
 
     expect(component).toHaveClass("dcui-progress__inner");
+  });
+
+  it("should render correct class (5)", () => {
+    props.variant = "danger";
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId("ProgressBar");
+
+    expect(component).toHaveClass("dcui-progress--danger");
+  });
+
+  it("changing max value should change aria-valuemax", () => {
+    props.maximumValue = 89;
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId("ProgressBarInner");
+    expect(component.getAttribute("aria-valuemax")).toEqual("89");
+  });
+
+  it("should render correct aria attributes (1)", () => {
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId("ProgressBarInner");
+    expect(component.getAttribute("aria-valuenow")).toEqual("50");
+  });
+  it("should render correct aria attributes (2)", () => {
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId("ProgressBarInner");
+    expect(component.getAttribute("aria-valuemin")).toEqual("0");
+  });
+  it("should render correct aria attributes (3)", () => {
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId("ProgressBarInner");
+    expect(component.getAttribute("aria-valuemax")).toEqual("100");
   });
 });
