3e665d6fc9e80e2dacb84c369887ff03fa9a2c8f
Daniel Corcoran <danielc92@outlook.com>
2021-06-03 20:38:54 +1000

    Breadcrumb baseline test

---
+++ src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -0,0 +1,47 @@
+// Generated with util/create-component.js
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Breadcrumbs from "./Breadcrumbs";
+import { BreadcrumbsProps } from "./Breadcrumbs.types";
+
+describe("Breadcrumb suite", () => {
+  let props: BreadcrumbsProps;
+
+  beforeEach(() => {
+    props = {
+      data: [
+        {
+          isCurrent: false,
+          linkHref: "#",
+          linkText: "Nuhten",
+        },
+        {
+          isCurrent: false,
+          linkHref: "#",
+          linkText: "Delore",
+        },
+        {
+          isCurrent: false,
+          linkHref: "#",
+          linkText: "Ipsum",
+        },
+        {
+          isCurrent: true,
+          linkHref: "#",
+          linkText: "Lorem",
+        },
+      ],
+    };
+  });
+
+  const renderComponent = () => render(<Breadcrumbs {...props} />);
+
+  it("should render base class", () => {
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId("Breadcrumbs");
+
+    expect(component).toHaveClass("dcui-breadcrumbs");
+  });
+});
