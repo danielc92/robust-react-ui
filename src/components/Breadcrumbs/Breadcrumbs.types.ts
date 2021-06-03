b2e67688f14cb4ae4b6ba5f69cf9c1d05bff92a7
Daniel Corcoran <danielc92@outlook.com>
2021-06-03 20:39:33 +1000

    Typing for breadcrumbs

---
+++ src/components/Breadcrumbs/Breadcrumbs.types.ts
@@ -0,0 +1,11 @@
+// Generated with util/create-component.js
+export interface Breadcrumb {
+  linkHref: string | null;
+  linkText: string;
+  isCurrent: boolean;
+}
+export interface BreadcrumbsProps {
+  ariaLabel?: string;
+  data: Array<Breadcrumb>;
+  size?: "small" | "medium" | "large";
+}
