777679c93bbd737521c51e78ffad3d06945d9a61
Daniel Corcoran <danielc92@outlook.com>
2021-06-05 15:42:36 +1000

    Added striped unit test

--- src/components/Table/Base/Table.test.tsx
+++ src/components/Table/Base/Table.test.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import Table from "./Table";
+import { TableProps } from "./Table.types";
 
 describe("Table suite", () => {
-  const renderComponent = () =>
+  const renderComponent = (partialProps?: Partial<TableProps>) =>
     render(
-      <Table>
+      <Table {...partialProps}>
         <Table.Head>
           <Table.Row>
             <Table.Header>Animals</Table.Header>
@@ -30,11 +31,18 @@ describe("Table suite", () => {
       </Table>
     );
 
-  it("should render corret base class", () => {
+  it("should render correct base class", () => {
     const { getByTestId } = renderComponent();
 
     const component = getByTestId("Table");
 
     expect(component).toHaveClass("dcui-table");
+  });
+  it("should render with striped modifier class", () => {
+    const { getByTestId } = renderComponent({ striped: true });
+
+    const component = getByTestId("Table");
+
+    expect(component).toHaveClass("dcui-table--striped");
   });
 });
