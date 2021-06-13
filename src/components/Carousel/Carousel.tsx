9af5bdb70e0018bcaccd9a4bebd4154733f5b60a
Daniel Corcoran <danielc92@outlook.com>
2021-06-13 20:02:18 +1000

    revert paths config to earlier state

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,9 +1,9 @@
 // Generated with util/create-component.js
 import React, { useEffect, useRef, useState } from 'react';
+import getClassNames from 'utils/getClassNames';
 import { CarouselProps } from './Carousel.types';
 import './Carousel.scss';
 import Typography from '../Typography';
-import getClassNames from './utils/getClassNames';
 
 const Carousel = ({
   ariaLabel,
