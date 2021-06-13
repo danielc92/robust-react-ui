f639ce8f81d7ae0beb5961a323b609eba5788abd
Daniel Corcoran <danielc92@outlook.com>
2021-06-13 19:58:52 +1000

    fix failing test

--- src/components/Carousel/Carousel.test.tsx
+++ src/components/Carousel/Carousel.test.tsx
@@ -1,27 +1,53 @@
 // Generated with util/create-component.js
-import React from "react";
-import { render } from "@testing-library/react";
+import React from 'react';
+import { render } from '@testing-library/react';
 
-import Carousel from "./Carousel";
-import { CarouselProps } from "./Carousel.types";
+import Carousel from './Carousel';
+import { CarouselProps } from './Carousel.types';
 
-describe("Test Component", () => {
+describe('Test Component', () => {
   let props: CarouselProps;
 
   beforeEach(() => {
     props = {
-      foo: "bar"
+      ariaLabel: 'Sample carousel',
+      slides: [
+        {
+          imageSrc: 'https://via.placeholder.com/1200x500/bf9c0d/ffffff',
+          imageAlt: 'alt text for an image',
+          href: 'https://github.com',
+          captionHeading:
+            'Do dolored. Alolore quis temris e adipisicing deserunt.',
+          captionText:
+            'Officia consectetur anim quis eu sit. Pariatur quis ea cupidatat ea irure ad laboris in esse minim excepteur. Dolor aliqua voluptate nisi ex ut tempor officia. Culpa ipsum voluptate ut amet elit cupidatat cillum aliqua fugiat nostrud exercitation pariatur deserunt. Id dolor veniam aliqua voluptate eiusmod pariatur excepteur dolor fugiat do et in.',
+        },
+        {
+          imageSrc: 'https://via.placeholder.com/1200x500/135691/ffffff',
+          imageAlt: 'alt text for an image',
+          href: 'https://github.com',
+          captionHeading: 'Esse non mollit laborum amet sint veniam.',
+          captionText:
+            'Magna nulla ea nulla ea commodo minim Lorem eu nisi laborum est nisi esse. Fugiat velit anim ea fugiat esse tempor esse. Lorem non sit ad enim laboris duis ad eu dolor ex velit nulla. Excepteur sint culpa irure ipsum ea amet ipsum. Sit reprehenderit exercitation Lorem consequat cupidatat mollit ullamco in ullamco exercitation veniam. Irure irure sint magna deserunt aliqua. Tempor consequat deserunt deserunt culpa incididunt et cillum sit deserunt.',
+        },
+        {
+          imageSrc: 'https://via.placeholder.com/1200x500/751754/ffffff',
+          imageAlt: 'alt text for an image',
+          href: 'https://github.com',
+          captionHeading: 'Ea elit labore laboris ex deserunt.',
+          captionText:
+            'Deserunt Lorem laboris mollit proident pariatur. Velit duis velit ut mollit laborum laborum eu cupidatat veniam deserunt culpa. Irure anim dolor et voluptate quis magna aliqua tempor nostrud labore consequat do. Quis nisi do enim mollit labore sint. Ullamco cupidatat commodo magna esse officia adipisicing aliquip ea ad magna magna excepteur.',
+        },
+      ],
     };
   });
 
   const renderComponent = () => render(<Carousel {...props} />);
 
-  it("should render foo text correctly", () => {
-    props.foo = "harvey was here";
+  it('should render with base class', () => {
     const { getByTestId } = renderComponent();
 
-    const component = getByTestId("Carousel");
+    const component = getByTestId('Carousel');
 
-    expect(component).toHaveTextContent("harvey was here");
+    expect(component).toHaveClass('dcui-carousel');
   });
 });
