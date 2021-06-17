1cad3c0226a130877c0c13b92d0788c93a8150c5
Daniel Corcoran <danielc92@outlook.com>
2021-06-17 21:35:09 +1000

    fix jest tests running again

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,23 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
-  roots: ["src"],
+  roots: ['src'],
   collectCoverage: true,
   verbose: true,
-  setupFilesAfterEnv: ["./jest.setup.ts"],
-  moduleFileExtensions: ["ts", "tsx", "js"],
-  testPathIgnorePatterns: ["node_modules/"],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testPathIgnorePatterns: ['node_modules/'],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  testMatch: ["**/*.test.(ts|tsx)"],
-  moduleDirectories: ["node_modules", "src"],
+  testMatch: ['**/*.test.(ts|tsx)'],
+  moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
     // Mocks out all these file formats when tests are run.
-    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "identity-obj-proxy",
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      'identity-obj-proxy',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 };
