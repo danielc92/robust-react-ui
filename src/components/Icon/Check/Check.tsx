183bf1ed0bb785508d4f398e7bc67d60afe0222e
Daniel Corcoran <danielc92@outlook.com>
2021-06-18 17:25:37 +1000

    Added several icons

---
+++ src/components/Icon/Check/Check.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Check = () => (
+  <>
+    <polyline points="20 6 9 17 4 12" />
+  </>
+);
+
+export default Check;
---
+++ src/components/Icon/Linkedin/index.ts
@@ -0,0 +1,3 @@
+import Linkedin from './Linkedin';
+
+export default Linkedin;
