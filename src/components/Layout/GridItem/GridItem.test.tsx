dd923e974560963d36ef2b736da968f7220aa266
Daniel Corcoran <danielc92@outlook.com>
2021-06-19 16:06:25 +1000

    GridItem draft

---
+++ src/components/Layout/GridItem/GridItem.test.tsx
@@ -0,0 +1,74 @@
+// Generated with util/create-component.js
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import GridItem from './GridItem';
+import { GridItemProps } from './GridItem.types';
+
+describe('Test Component', () => {
+  let props: GridItemProps;
+
+  beforeEach(() => {
+    props = {};
+  });
+
+  const renderComponent = () => render(<GridItem {...props} />);
+
+  it('should render with primary class by default', () => {
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId('GridItem');
+
+    expect(component).toHaveClass('dcui-grid__item--primary');
+  });
+  it('should render with padding class', () => {
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId('GridItem');
+
+    expect(component).toHaveClass('dcui-grid__item--padding-x-s');
+    expect(component).toHaveClass('dcui-grid__item--padding-y-s');
+  });
+  it('should render with correct padding class when padding props present', () => {
+    props.paddingX = 'l';
+    props.paddingY = 'xs';
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId('GridItem');
+
+    expect(component).toHaveClass('dcui-grid__item--padding-x-l');
+    expect(component).toHaveClass('dcui-grid__item--padding-y-xs');
+  });
+  it('should render with danger class', () => {
+    props.variant = 'danger';
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId('GridItem');
+
+    expect(component).toHaveClass('dcui-grid__item--danger');
+  });
+  it('should render with dark class', () => {
+    props.variant = 'dark';
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId('GridItem');
+
+    expect(component).toHaveClass('dcui-grid__item--dark');
+  });
+  it('should render with light class', () => {
+    props.variant = 'light';
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId('GridItem');
+
+    expect(component).toHaveClass('dcui-grid__item--light');
+  });
+  it('should render with success class', () => {
+    props.variant = 'success';
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId('GridItem');
+
+    expect(component).toHaveClass('dcui-grid__item--success');
+  });
+});
---
+++ src/components/Layout/GridItem/GridItem.tsx
@@ -0,0 +1,46 @@
+// Generated with util/create-component.js
+import React from 'react';
+import getClassNames from 'robust-react-ui/utils/getClassNames';
+import { GridItemProps } from './GridItem.types';
+import './GridItem.scss';
+
+const GridItem = ({
+  children,
+  config,
+  variant = 'primary',
+  paddingX = 's',
+  paddingY = 's',
+}: GridItemProps) => {
+  const style: React.CSSProperties = config
+    ? {
+        gridRowStart: config.rowStart,
+        gridRowEnd: config.rowStart + config.height,
+        gridColumnStart: config.colStart,
+        gridColumnEnd: config.colStart + config.width,
+      }
+    : {};
+
+  // shrink when col exceeds tablet width (4 fr)
+  const shrinkOnTablet = config && config.width > 4;
+
+  // shrink when col exceeds mobil width (2 fr)
+  const shrinkOnMobile = config && config.width > 1;
+  return (
+    <div
+      data-testid="GridItem"
+      className={getClassNames({
+        'dcui-grid__item': true,
+        [`dcui-grid__item--${variant}`]: true,
+        [`dcui-grid__item--padding-x-${paddingX}`]: true,
+        [`dcui-grid__item--padding-y-${paddingY}`]: true,
+        'dcui-grid__item--shrink-tablet': shrinkOnTablet,
+        'dcui-grid__item--shrink-mobile': shrinkOnMobile && !shrinkOnTablet,
+      })}
+      style={style}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default GridItem;
