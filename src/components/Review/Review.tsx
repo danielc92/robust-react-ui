d3e0404f3d716fcfc3bcd00379a562c8dc5ff43f
Daniel Corcoran <danielc92@outlook.com>
2021-06-25 22:04:31 +1000

    feat: review function baseline

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -1,75 +1,112 @@
 // Generated with util/create-component.js
-import React from "react";
+import React from 'react';
 
-import { ReviewProps } from "./Review.types";
+import { ReviewProps } from './Review.types';
 
-import "./Review.scss";
-import Star from "../Icon/Star";
+import './Review.scss';
+import Star from '../Icon/Star';
+import Crosshair from '../Icon/Crosshair';
+import X from '../Icon/X';
+import Icon from '../Icon';
 
-const Review = ({ foo }: ReviewProps) => (
-<form action="#" id="star_rating">
- <input value="0" id="star0" checked
-	type="radio" name="rating" className="visuallyhidden"/>
-	<label htmlFor="star0">
-		<span className="visuallyhidden">0 Stars</span>
-		<svg viewBox="0 0 512 512">
-			<g stroke-width="70" stroke-linecap="square">
-				<path d="M91.5,442.5 L409.366489,124.633512"></path>
-				<path d="M90.9861965,124.986197 L409.184248,443.184248"></path>
-			</g>
-		</svg>
-	</label>
+const Review = ({ formId = 'star_rating' }: ReviewProps) => (
+  <form id={formId}>
+    <input
+      value="0"
+      id="star0"
+      checked
+      type="radio"
+      name="rating"
+      className="rrui-review--vis-hidden"
+    />
+    <label htmlFor="star0">
+      <span className="rrui-review--vis-hidden">0 Stars</span>
+      <svg viewBox="0 0 24 24">
+        <X />
+      </svg>
+    </label>
 
-	<input value="1" id="star1"
-		type="radio" name="rating" className="visuallyhidden"/>
-	<label htmlFor="star1">
-		<span className="visuallyhidden">1 Star</span>
-		<svg viewBox=" 0 0 24 24">
-            <Star/>
-        </svg>
-	</label>
+    <input
+      value="1"
+      id="star1"
+      type="radio"
+      name="rating"
+      className="rrui-review--vis-hidden"
+    />
+    <label htmlFor="star1">
+      <span className="rrui-review--vis-hidden">1 Star</span>
+      <svg viewBox="0 0 24 24">
+        <Star />
+      </svg>
+    </label>
 
-	<input value="2" id="star2"
-		type="radio" name="rating" className="visuallyhidden"/>
-	<label htmlFor="star2">
-		<span className="visuallyhidden">2 Stars</span>
-		<svg viewBox=" 0 0 24 24">
-            <Star/>
-        </svg>
-	</label>
+    <input
+      value="2"
+      id="star2"
+      type="radio"
+      name="rating"
+      className="rrui-review--vis-hidden"
+    />
+    <label htmlFor="star2">
+      <span className="rrui-review--vis-hidden">2 Stars</span>
+      <svg viewBox="0 0 24 24">
+        <Star />
+      </svg>
+    </label>
 
-	<input value="3" id="star3"
-		type="radio" name="rating" className="visuallyhidden"/>
-	<label htmlFor="star3">
-		<span className="visuallyhidden">3 Stars</span>
-		<svg viewBox=" 0 0 24 24">
-            <Star/>
-        </svg>
-	</label>
+    <input
+      value="3"
+      id="star3"
+      type="radio"
+      name="rating"
+      className="rrui-review--vis-hidden"
+    />
+    <label htmlFor="star3">
+      <span className="rrui-review--vis-hidden">3 Stars</span>
+      <svg viewBox="0 0 24 24">
+        <Star />
+      </svg>
+    </label>
 
-	<input value="4" id="star4"
-		type="radio" name="rating" className="visuallyhidden"/>
-	<label htmlFor="star4">
-		<span className="visuallyhidden">4 Stars</span>
-		<svg viewBox=" 0 0 24 24">
-            <Star/>
-        </svg>
-	</label>
+    <input
+      value="4"
+      id="star4"
+      type="radio"
+      name="rating"
+      className="rrui-review--vis-hidden"
+    />
+    <label htmlFor="star4">
+      <span className="rrui-review--vis-hidden">4 Stars</span>
+      <svg viewBox="0 0 24 24">
+        <Star />
+      </svg>
+    </label>
 
-	<input value="5" id="star5"
-		type="radio" name="rating" className="visuallyhidden">
-	<label htmlFor="star5">
-		<span className="visuallyhidden">5 Stars</span>
-        <svg viewBox=" 0 0 24 24">
-            <Star/>
-        </svg>
-	</label>
+    <input
+      value="5"
+      id="star5"
+      type="radio"
+      name="rating"
+      className="rrui-review--vis-hidden"
+    />
+    <label htmlFor="star5">
+      <span className="rrui-review--vis-hidden">5 Stars</span>
+      <svg viewBox="0 0 24 24">
+        <Star />
+      </svg>
+    </label>
 
-	<button type="submit" className="btn-small visuallyhidden focusable">Submit rating</button>
+    <button
+      onClick={() => console.log('WHOA')}
+      style={{ background: 'blue' }}
+      type="submit"
+      className="btn-small rrui-review--vis-hidden focusable"
+    >
+      Submit rating
+    </button>
 
-	<output></output>
-</form>
-    );
+    <output></output>
+  </form>
+);
 
 export default Review;
-
