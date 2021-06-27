4eaef4dd77f5067aaf18662a6665101b718a5483
Daniel Corcoran <danielc92@outlook.com>
2021-06-27 19:21:59 +1000

    docs: update navigation bar data

--- src/components/NavigationBar/NavigationBar.stories.tsx
+++ src/components/NavigationBar/NavigationBar.stories.tsx
@@ -6,18 +6,86 @@ export default {
   title: 'Components/NavigationBar',
 };
 
-const sampleLink = 'https://github.com';
 const navData = [
   {
     id: 1,
     parentId: null,
-    linkName: 'Google',
-    linkHref: sampleLink,
+    linkName: 'Home',
+    linkHref: 'https://github.com/danielc92/robust-react-ui',
+  },
+
+  {
+    id: 17,
+    parentId: null,
+    linkName: 'Vegetables',
+    children: [
+      {
+        id: 18,
+        parentId: 17,
+        linkName: 'Carrot',
+        linkHref: 'https://github.com/danielc92/robust-react-ui',
+      },
+      {
+        id: 19,
+        parentId: 17,
+        linkName: 'Carrot',
+        linkHref: 'https://github.com/danielc92/robust-react-ui',
+      },
+      {
+        id: 20,
+        parentId: 17,
+        linkName: 'Radish',
+        linkHref: 'https://github.com/danielc92/robust-react-ui',
+      },
+      {
+        id: 21,
+        parentId: 17,
+        linkName: 'Potato',
+        linkHref: 'https://github.com/danielc92/robust-react-ui',
+      },
+      {
+        id: 22,
+        parentId: 17,
+        linkName: 'Eggplant',
+        linkHref: 'https://github.com/danielc92/robust-react-ui',
+      },
+    ],
+  },
+  {
+    id: 23,
+    parentId: null,
+    linkName: 'Nuts',
+    children: [
+      {
+        id: 24,
+        parentId: 23,
+        linkName: 'Almonds',
+        linkHref: 'https://github.com/danielc92/robust-react-ui',
+      },
+      {
+        id: 25,
+        parentId: 23,
+        linkName: 'Peanuts',
+        linkHref: 'https://github.com/danielc92/robust-react-ui',
+      },
+      {
+        id: 26,
+        parentId: 23,
+        linkName: 'Cashew',
+        linkHref: 'https://github.com/danielc92/robust-react-ui',
+      },
+      {
+        id: 27,
+        parentId: 23,
+        linkName: 'Macadamia',
+        linkHref: 'https://github.com/danielc92/robust-react-ui',
+      },
+    ],
   },
   {
     id: 2,
     parentId: null,
-    linkName: 'Fruit Listing',
+    linkName: 'Fruits',
     children: [
       {
         id: 6,
@@ -28,20 +96,20 @@ const navData = [
             id: 10,
             parentId: 6,
             linkName: 'Apple',
-            linkHref: sampleLink,
+            linkHref: 'https://github.com/danielc92/robust-react-ui',
           },
           {
             id: 11,
             parentId: 6,
             linkName: 'Strawberries',
-            linkHref: sampleLink,
+            linkHref: 'https://github.com/danielc92/robust-react-ui',
           },
 
           {
             id: 13,
             parentId: 6,
             linkName: 'Kiwi',
-            linkHref: sampleLink,
+            linkHref: 'https://github.com/danielc92/robust-react-ui',
           },
         ],
       },
@@ -54,25 +122,25 @@ const navData = [
             id: 54,
             parentId: 5,
             linkName: 'Lemon',
-            linkHref: sampleLink,
+            linkHref: 'https://github.com/danielc92/robust-react-ui',
           },
           {
             id: 6548,
             parentId: 5,
             linkName: 'Mango',
-            linkHref: sampleLink,
+            linkHref: 'https://github.com/danielc92/robust-react-ui',
           },
           {
             id: 289654,
             parentId: 5,
             linkName: 'Lime',
-            linkHref: sampleLink,
+            linkHref: 'https://github.com/danielc92/robust-react-ui',
           },
           {
             id: 2544,
             parentId: 5,
             linkName: 'Grape',
-            linkHref: sampleLink,
+            linkHref: 'https://github.com/danielc92/robust-react-ui',
           },
         ],
       },
@@ -81,15 +149,9 @@ const navData = [
         id: 4,
         parentId: 2,
         linkName: 'Other',
-        linkHref: sampleLink,
+        linkHref: 'https://github.com/danielc92/robust-react-ui',
       },
     ],
-  },
-  {
-    id: 3,
-    parentId: null,
-    linkName: 'About us',
-    linkHref: sampleLink,
   },
 ];
 
